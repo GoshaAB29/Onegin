9c691a092f1ef651eb3dc435115468de6e8f3f79
Gosha <89145061+GoshaAB29@users.noreply.github.com>
2021-10-03 00:02:43 +0300

    fix bugs

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
-#include "header/onegin.h"
+#include "include/onegin.h"
 #include <iostream>
 
+#define DEBUG
+
+/* TODO:
+1) Correct some issues mention in comments (ok)
+2) Docs
+3) Debug program (ok)
+4) Write your own sort
+*/
 int main(int argc, char *argv[])
 {
     setlocale (LC_ALL, "ru_Ru.cp1251");
 
     Text text = {};
 
-    read_from_file (&text, "test1.txt"); //...argv[1]);
+    #ifdef DEBUG
+        argv[1] = "test1.txt";
+    #endif
+
+    text_init (&text, argv[1]);
     do_sorting (&text);
     write_to_file (&text);
 
